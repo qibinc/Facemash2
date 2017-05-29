02f7c2ad4f2bcd4bf8352aa30b9bfe45c327fff4
skeletondyh <dingyh15@163.com>
2017-05-29 12:26:55 +0800

    Add client files

---
+++ client/untitled2/data.cpp
@@ -0,0 +1,100 @@
+#include "data.h"
+
+Photo::Photo()
+{
+
+}
+
+Photo::Photo(const QString &title):
+    _title(title)
+{
+
+}
+
+Photo::Photo(const QString &title, double points):
+    _title(title), _points(points)
+{
+
+}
+
+Photo::Photo(const QString &filepath, const QString &title, const QString &uploader, double points):
+    _photo(filepath), _title(title), _uploader(uploader), _points(points)
+{
+
+}
+
+QDataStream &operator<<(QDataStream & out, const Photo& photo)
+{
+    out<<photo._photo;
+    out<<photo._title;
+    out<<photo._uploader;
+    out<<photo._points;
+    return out;
+}
+
+QDataStream &operator>>(QDataStream& in, Photo& photo)
+{
+    in>>photo._photo;
+    in>>photo._title;
+    in>>photo._uploader;
+    in>>photo._points;
+    return in;
+}
+
+
+Group::Group()
+{
+
+}
+
+
+QDataStream &operator<<(QDataStream & out, const Group& group)
+{
+    out<<group._date;
+    out<<group._photonum;
+    out<<group._photos;
+    return out;
+}
+
+QDataStream &operator>>(QDataStream & in, Group& group)
+{
+    in>>group._date;
+    in>>group._photonum;
+    in>>group._photos;
+    return in;
+}
+
+
+User::User():
+    _clienttype(DEFAULTc),_servertype(DEFAULTs)
+{
+
+}
+
+QDataStream &operator <<(QDataStream & out, const User& user)
+{
+    out<<user._username;
+    out<<user._password;
+    out<<user._datetime;
+    out<<(qint32)user._clienttype;
+    out<<(qint32)user._servertype;
+    out<<user._log;
+    out<<user._groupnum;
+    out<<user._groups;
+    out<<user._config;
+    return out;
+}
+
+QDataStream &operator >>(QDataStream &in, User& user)
+{
+    in>>user._username;
+    in>>user._password;
+    in>>user._datetime;
+    in>>(qint32&)user._clienttype;
+    in>>(qint32&)user._servertype;
+    in>>user._log;
+    in>>user._groupnum;
+    in>>user._groups;
+    in>>user._config;
+    return in;
+}
---
+++ client/untitled2/myclient.h
@@ -0,0 +1,63 @@
+#ifndef MYCLIENT_H
+#define MYCLIENT_H
+
+#include <QtNetwork/QTcpSocket>
+#include <QFile>
+#include <QDir>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QDateTime>
+#include "data.h"
+
+class MyClient : public QObject
+{
+    Q_OBJECT
+
+private:
+    QTcpSocket* _readwritesocket;
+    qint64 totalBytes;//总字节数
+    qint64 BytesReceived;//已接收字节数
+
+    QString KeepPath;//保存根目录
+    //QFile *file;
+
+    //User *_user;
+    QByteArray inArray;
+
+    QString UserName;
+    //QString _password;
+
+    QString HostIP;
+
+    void SavePhotos(User *user);
+    //void SaveBigPhoto(User *user);
+    void SaveLog(User *user);
+    void UpdatePoints(User *user);
+
+private slots:
+    void DealWithMessage(User *user);
+    void ReceiveMessage();
+
+signals:
+    Received(User*);
+    PhotosSaved(QList<QString>*);
+    LogSaved();
+    PointsUpdated();
+
+public:
+    explicit MyClient(QObject* parent = 0);
+    ~MyClient();
+    void SetPath(const QString& path);
+    void SetIP(const QString& hostip);
+    void SetupConnection();
+
+    void EvalPhoto(const QString& date, const QString& title, double points);
+    void LogIn(const QString& username);
+    void LogOut();
+    void AskforLog();
+    void UploadOnePhoto(const QString& photopath);
+    void AskforBigPhoto(const QString& date, const QString& title);
+
+};
+
+#endif // MYCLIENT_H
